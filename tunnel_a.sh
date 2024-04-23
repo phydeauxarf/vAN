26dae2bea6a06930fc16f11143b08cf9fafc9747
phydeauxarf <bill@beesley.org>
2024-04-23 17:27:53 -0500

    Update tunnel_a.sh

--- tunnel_a.sh
+++ tunnel_a.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 ip l2tp add tunnel tunnel_id 1000 peer_tunnel_id 2000 \
               encap udp local 10.10.1.10 remote 10.10.1.20 \
-              udp_sport 6000 udp_rport 5000
+              udp_sport 6000 udp_dport 5000
 ip l2tp add session tunnel_id 1000 session_id 3000 \
               peer_session_id 4000
+
+ip l2tp show tunnel 
 
 brctl addbr br0
 brctl addiff br0 eth0
